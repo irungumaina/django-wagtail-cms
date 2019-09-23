9fdc276b37d4d13980cef521826f6d91e1629f86
irungumaina <kirathimo2010@gmail.com>
2019-09-23 16:32:00 +0300

    created db connection

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -108,10 +108,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'irungu_maina',
+        'USER': 'postgres',
+        'PASSWORD': 'ITAH2015',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
